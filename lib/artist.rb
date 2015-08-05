0e3c5744b3965ce2a3bc64a1c600e2d310142ba7
supzann3 <susan.hehe01@gmail.com>
2015-08-05 17:07:00 -0400

    all done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,12 @@
 class Artist
   # code goes here
+ 
+def initialize(name)
+  @name = name
+end
+
+def name 
+  @name
+end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,28 @@
 class Song
   # code goes here
+    def title=(title)
+    @title=title
+  end
+
+  def title
+    @title
+  end
+
+  def artist=(artist)
+    @artist=artist
+  end
+
+  def artist
+  @artist
+  end
+
+  def serialize
+    file_name=@title.downcase.tr!(" ","_")
+    temp_file = Tempfile.new(["#{file_name}",".txt"],"tmp")     
+    temp_file.write ("#{artist.name} - #{@title}")
+    temp_file.rewind
+    temp_file.read
+    temp_file.close
+  end
+
 end
